2a87b6e6b715a388c474658be0d6bad8fffe93fd
Sebastian Dang <sebastiandang@codeaurora.org>
2020-11-18 11:12:03 -0800

    mmrm-test: Verify clock sources & voltage corners.
    - Use mmrm test dtsi to retrieve clks (clk_get).
    - Set rate for available voltage corners.
    - Set rate to 0 before deregistering.
    - Verify mmrm driver behavior for qcom_clk_get_voltage.
    - Fix minor formatting.
    
    Change-Id: I117e45eceb9514752e7a8d7147c814ec1254933b

--- mmrm_test.c
+++ mmrm_test.c
@@ -17,6 +17,8 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/platform_device.h>
 
 #include <soc/qcom/socinfo.h>
@@ -27,63 +29,70 @@
 
 static int mmrm_test_probe(struct platform_device *pdev)
 {
-  int soc_id;
+	int soc_id;
 
-  pr_info("%s: Validating mmrm on target: %s\n", __func__, socinfo_get_id_string());
+	// Check if of_node is found
+	if (!of_device_is_compatible(pdev->dev.of_node, "qcom,msm-mmrm-test")) {
+		pr_info("No compatible device node\n");
+		return 1;
+	}
 
-  // Get socid to get known mmrm configurations
-  soc_id = socinfo_get_id();
-  switch (soc_id)
-  {
-  case 415:
-    test_mmrm_client(pdev, MMRM_TEST_LAHAINA, MMRM_TEST_LAHAINA_NUM_CLK_CLIENTS);
-    break;
-  default:
-    pr_info("%s: Not supported for soc_id %d [Target %s]\n", __func__, soc_id, socinfo_get_id_string());
-    return -ENODEV;
-  }
+	pr_info("%s: Validating mmrm on target: %s\n", __func__, socinfo_get_id_string());
 
-  return 0;
+	// Get socid to get known mmrm configurations
+	soc_id = socinfo_get_id();
+	switch (soc_id) {
+	case 415: /* LAHAINA */
+		test_mmrm_client(pdev, MMRM_TEST_LAHAINA, MMRM_TEST_LAHAINA_NUM_CLK_CLIENTS);
+		break;
+	default:
+		pr_info("%s: Not supported for soc_id %d [Target %s]\n",
+			__func__,
+			soc_id,
+			socinfo_get_id_string());
+		return -ENODEV;
+	}
+
+	return 0;
 }
 
-int mmrm_test_remove(struct platform_device *pdev)
-{
-  return 0;
-}
+int mmrm_test_remove(struct platform_device *pdev) { return 0; }
 
-static struct platform_driver mmrm_test_driver = {
-    .probe = mmrm_test_probe,
-    .remove = mmrm_test_remove,
-    .driver = {
-        .name = MODULE_NAME,
-        .owner = THIS_MODULE,
-    },
+static const struct of_device_id mmrm_test_dt_match[] = {
+	{.compatible = "qcom,msm-mmrm-test"}, {} // empty
 };
 
-static void mmrm_test_release(struct device *dev)
-{
-  return;
-}
+static struct platform_driver mmrm_test_driver = {
+	.probe = mmrm_test_probe,
+	.remove = mmrm_test_remove,
+	.driver = {
+			.name = MODULE_NAME,
+			.owner = THIS_MODULE,
+			.of_match_table = mmrm_test_dt_match,
+		},
+};
+
+static void mmrm_test_release(struct device *dev) { return; }
 
 static struct platform_device mmrm_test_device = {
-    .name = MODULE_NAME,
-    .id = -1,
-    .dev = {
-        .release = mmrm_test_release,
-    },
+	.name = MODULE_NAME,
+	.id = -1,
+	.dev = {
+			.release = mmrm_test_release,
+		},
 };
 
 static int __init mmrm_test_init(void)
 {
-  platform_device_register(&mmrm_test_device);
-  return platform_driver_register(&mmrm_test_driver);
+	platform_device_register(&mmrm_test_device);
+	return platform_driver_register(&mmrm_test_driver);
 }
 module_init(mmrm_test_init);
 
 static void __exit mmrm_test_exit(void)
 {
-  platform_driver_unregister(&mmrm_test_driver);
-  platform_device_unregister(&mmrm_test_device);
+	platform_driver_unregister(&mmrm_test_driver);
+	platform_device_unregister(&mmrm_test_device);
 }
 module_exit(mmrm_test_exit);
 
--- mmrm_test_internal.c
+++ mmrm_test_internal.c
@@ -19,199 +19,234 @@
 
 #define MMRM_TEST_MAX_CLK_CLIENTS 5
 
-struct mmrm_test_clk_client
-{
-  struct mmrm_clk_client_desc clk_client_desc;
-  unsigned long svsl1;
-  unsigned long nom;
+enum mmrm_test_vdd_levels {
+	MMRM_TEST_VDD_LEVEL_SVS_L1,
+	MMRM_TEST_VDD_LEVEL_NOM,
+	MMRM_TEST_VDD_LEVEL_TURBO,
+	MMRM_TEST_VDD_LEVEL_MAX
+};
+
+struct mmrm_test_clk_client {
+	struct mmrm_clk_client_desc clk_client_desc;
+	unsigned long clk_rate[MMRM_TEST_VDD_LEVEL_MAX];
 };
 
 static struct mmrm_test_clk_client mmrm_test_clk_client_list[][MMRM_TEST_MAX_CLK_CLIENTS] = {
-    /* LAHAINA */
-    {
-        {{MMRM_CLIENT_DOMAIN_CAMERA, 42, "CAM_CC_IFE_0_CLK_SRC", NULL}, 675, 785},
-        {{MMRM_CLIENT_DOMAIN_CAMERA, 51, "CAM_CC_IFE_1_CLK_SRC", NULL}, 675, 785},
-        {{MMRM_CLIENT_DOMAIN_CVP, 8, "VIDEO_CC_MVS1_CLK_SRC", NULL}, 1500, 1650},
-        {{MMRM_CLIENT_DOMAIN_DISPLAY, 41, "DISP_CC_MDSS_MDP_CLK_SRC", NULL}, 375, 500},
-        {{MMRM_CLIENT_DOMAIN_VIDEO, 3, "VIDEO_CC_MVS0_CLK_SRC", NULL}, 1098, 1332},
-    },
+	/* LAHAINA */
+	{
+		{{MMRM_CLIENT_DOMAIN_CAMERA, 42, "cam_cc_ife_0_clk_src", NULL},
+			{600000000, 720000000, 720000000}},
+		{{MMRM_CLIENT_DOMAIN_CAMERA, 51, "cam_cc_ife_1_clk_src", NULL},
+			{600000000, 720000000, 720000000}},
+		{{MMRM_CLIENT_DOMAIN_CVP, 8, "video_cc_mvs1_clk_src", NULL},
+			{1098000000, 1332000000, 1332000000}},
+		{{MMRM_CLIENT_DOMAIN_DISPLAY, 41, "disp_cc_mdss_mdp_clk_src", NULL},
+			{345000000, 460000000, 460000000}},
+		{{MMRM_CLIENT_DOMAIN_VIDEO, 3, "video_cc_mvs0_clk_src", NULL},
+			{1098000000, 1332000000, 1332000000}},
+	},
 };
 
 static int test_mmrm_client_callback(struct mmrm_client_notifier_data *notifier_data)
 {
-  // TODO: Test callback here
-  return 0;
+	// TODO: Test callback here
+	return 0;
 }
 
 static struct mmrm_client *test_mmrm_client_register(struct mmrm_client_desc *desc)
 {
-  struct mmrm_client *client;
+	struct mmrm_client *client;
 
-  if (!desc)
-  {
-    pr_info("%s: Invalid input\n", __func__);
-    return NULL;
-  }
+	if (!desc) {
+		pr_info("%s: Invalid input\n", __func__);
+		return NULL;
+	}
 
-  pr_info("%s: domain(%d) cid(%d) name(%s) type(%d) pri(%d) pvt(%p) notifier(%p)\n",
-          __func__,
-          desc->client_info.desc.client_domain,
-          desc->client_info.desc.client_id,
-          desc->client_info.desc.name,
-          desc->client_type,
-          desc->priority,
-          desc->pvt_data,
-          desc->notifier_callback_fn);
+	pr_info("%s: domain(%d) cid(%d) name(%s) type(%d) pri(%d) pvt(%p) "
+		"notifier(%p)\n",
+		__func__,
+		desc->client_info.desc.client_domain,
+		desc->client_info.desc.client_id,
+		desc->client_info.desc.name,
+		desc->client_type,
+		desc->priority,
+		desc->pvt_data,
+		desc->notifier_callback_fn);
 
-  pr_info("%s: Registering mmrm client %s\n", __func__, desc->client_info.desc.name);
-  client = mmrm_client_register(desc);
-  if (client == NULL)
-  {
-    pr_info("%s: Failed to register mmrm client %s\n", __func__, desc->client_info.desc.name);
-    return NULL;
-  }
-  pr_info("%s: cuid(%d)\n", __func__, client->client_uid);
-  return client;
+	pr_info("%s: Registering mmrm client %s\n", __func__, desc->client_info.desc.name);
+	client = mmrm_client_register(desc);
+	if (client == NULL) {
+		pr_info("%s: Failed to register mmrm client %s\n",
+			__func__,
+			desc->client_info.desc.name);
+		return NULL;
+	}
+	pr_info("%s: cuid(%d)\n", __func__, client->client_uid);
+	return client;
 }
 
 static int test_mmrm_client_deregister(struct mmrm_client *client)
 {
-  int rc;
+	int rc;
 
-  if (!client)
-  {
-    pr_info("%s: Invalid input\n", __func__);
-    return -EINVAL;
-  }
+	if (!client) {
+		pr_info("%s: Invalid input\n", __func__);
+		return -EINVAL;
+	}
 
-  pr_info("%s: cuid(%d) Deregistering mmrm client\n", __func__, client->client_uid);
-  rc = mmrm_client_deregister(client);
-  if (rc != 0)
-  {
-    pr_info("%s: cuid(%d) Failed to deregister mmrm client with %d\n", __func__, client->client_uid, rc);
-    return rc;
-  }
-  return rc;
+	pr_info("%s: cuid(%d) Deregistering mmrm client\n", __func__, client->client_uid);
+	rc = mmrm_client_deregister(client);
+	if (rc != 0) {
+		pr_info("%s: cuid(%d) Failed to deregister mmrm client with %d\n",
+			__func__,
+			client->client_uid,
+			rc);
+		return rc;
+	}
+	return rc;
 }
 
-static int test_mmrm_client_set_value(struct mmrm_client *client, struct mmrm_client_data *client_data, unsigned long val)
+static int test_mmrm_client_set_value(
+	struct mmrm_client *client, struct mmrm_client_data *client_data, unsigned long val)
 {
-  int rc;
+	int rc;
 
-  if (!client || !client_data)
-  {
-    pr_info("%s: Invalid input\n", __func__);
-    return -EINVAL;
-  }
+	if (!client || !client_data) {
+		pr_info("%s: Invalid input\n", __func__);
+		return -EINVAL;
+	}
 
-  pr_info("%s: cuid(%d) Setting value for mmrm client\n", __func__, client->client_uid);
-  rc = mmrm_client_set_value(client, client_data, val);
-  if (rc != 0)
-  {
-    pr_info("%s: cuid(%d) Failed to set value for mmrm client with %d\n", __func__, client->client_uid, rc);
-    return rc;
-  }
-  return rc;
+	pr_info("%s: cuid(%d) Setting value(%d) for mmrm client\n",
+		__func__,
+		client->client_uid,
+		val);
+	rc = mmrm_client_set_value(client, client_data, val);
+	if (rc != 0) {
+		pr_info("%s: cuid(%d) Failed to set value(%d) for mmrm client with %d\n",
+			__func__,
+			client->client_uid,
+			val,
+			rc);
+		return rc;
+	}
+	return rc;
 }
 
 static int test_mmrm_client_get_value(struct mmrm_client *client, struct mmrm_client_res_value *val)
 {
-  int rc;
+	int rc;
 
-  if (!client || !val)
-  {
-    pr_info("%s: Invalid input\n", __func__);
-    return -EINVAL;
-  }
+	if (!client || !val) {
+		pr_info("%s: Invalid input\n", __func__);
+		return -EINVAL;
+	}
 
-  pr_info("%s: cuid(%d) Getting value for mmrm client\n", __func__, client->client_uid);
-  rc = mmrm_client_get_value(client, val);
-  if (rc != 0)
-  {
-    pr_info("%s: cuid(%d) Failed to get value for mmrm client with %d\n", __func__, client->client_uid, rc);
-    return rc;
-  }
-  pr_info("%s: cuid(%d) min(%d) cur(%d) max(%d)\n", __func__, client->client_uid, val->min, val->cur, val->max);
-  return rc;
+	pr_info("%s: cuid(%d) Getting value for mmrm client\n", __func__, client->client_uid);
+	rc = mmrm_client_get_value(client, val);
+	if (rc != 0) {
+		pr_info("%s: cuid(%d) Failed to get value for mmrm client with %d\n",
+			__func__,
+			client->client_uid,
+			rc);
+		return rc;
+	}
+	pr_info("%s: cuid(%d) min(%d) cur(%d) max(%d)\n",
+		__func__,
+		client->client_uid,
+		val->min,
+		val->cur,
+		val->max);
+	return rc;
 }
 
 void test_mmrm_client(struct platform_device *pdev, int index, int count)
 {
-  struct clk *clk;                      // clk struct
-  struct mmrm_client *client;           // mmrm client
-  struct mmrm_client_data client_data;  // mmrm client data
-  struct mmrm_client_res_value res_val; // mmrm get_val
-  unsigned long val;                    // mmrm set_val
+	struct clk *clk; // clk struct
+	struct mmrm_client *client; // mmrm client
+	struct mmrm_client_data client_data; // mmrm client data
+	struct mmrm_client_res_value res_val; // mmrm get_val
+	int level; // mmrm set_val
+	unsigned long val; // mmrm set_val
 
-  int i, pass_count;
-  int rc = 0;
+	int i, pass_count;
+	int rc = 0;
 
-  pr_info("%s: Running client tests\n", __func__);
+	pr_info("%s: Running client tests\n", __func__);
 
-  // Run nominal test for each individual clock source
-  for (i = 0, pass_count = 0; i < count; i++)
-  {
-    // Create callback used to pass resource data to client
-    struct mmrm_client_notifier_data notifier_data = {
-        MMRM_CLIENT_RESOURCE_VALUE_CHANGE, // cb_type
-        {{0, 0}},                          // cb_data (old_val, new_val)
-        NULL};                             // pvt_data
+	// Run nominal test for each individual clock source
+	for (i = 0, pass_count = 0; i < count; i++) {
+		// Create callback used to pass resource data to client
+		struct mmrm_client_notifier_data notifier_data = {
+			MMRM_CLIENT_RESOURCE_VALUE_CHANGE, // cb_type
+			{{0, 0}}, // cb_data (old_val, new_val)
+			NULL}; // pvt_data
 
-    // Create client descriptor
-    struct mmrm_client_desc desc = {
-        MMRM_CLIENT_CLOCK,                                     // client type
-        {mmrm_test_clk_client_list[index][i].clk_client_desc}, // clock client descriptor
-        MMRM_CLIENT_PRIOR_HIGH,                                // client priority
-        notifier_data.pvt_data,                                // pvt_data
-        test_mmrm_client_callback};                            // callback fn
+		// Create client descriptor
+		struct mmrm_client_desc desc = {MMRM_CLIENT_CLOCK, // client type
+			{mmrm_test_clk_client_list[index][i]
+					.clk_client_desc}, // clock client descriptor
+			MMRM_CLIENT_PRIOR_HIGH, // client priority
+			notifier_data.pvt_data, // pvt_data
+			test_mmrm_client_callback}; // callback fn
 
-    // Get dummy clk
-    clk = clk_get(&pdev->dev, "dummy_clk");
-    desc.client_info.desc.clk = clk;
+		// Get clk
+		clk = clk_get(&pdev->dev, (const char *)&desc.client_info.desc.name);
+		if (IS_ERR_OR_NULL(clk)) {
+			pr_info("%s: Failed clk_get for %s\n",
+				__func__,
+				desc.client_info.desc.name);
+			rc = -EINVAL;
+			goto err_clk;
+		}
+		desc.client_info.desc.clk = clk;
+		pr_info("%s: clk(%pK)\n", __func__, clk);
 
-    // Register client
-    client = test_mmrm_client_register(&desc);
-    if (client == NULL)
-    {
-      rc = -EINVAL;
-      goto err_register;
-    }
+		// Register client
+		client = test_mmrm_client_register(&desc);
+		if (client == NULL) {
+			rc = -EINVAL;
+			goto err_register;
+		}
 
-    // Set values (Use reserve only)
-    client_data = (struct mmrm_client_data){0, MMRM_CLIENT_DATA_FLAG_RESERVE_ONLY};
+		// Set values (Use reserve only)
+		client_data = (struct mmrm_client_data){0, MMRM_CLIENT_DATA_FLAG_RESERVE_ONLY};
+		for (level = 0; level < MMRM_TEST_VDD_LEVEL_MAX; level++) {
+			val = mmrm_test_clk_client_list[index][i].clk_rate[level];
+			rc = test_mmrm_client_set_value(client, &client_data, val);
+			if (rc != 0)
+				goto err_setval;
+		}
 
-    val = mmrm_test_clk_client_list[index][i].svsl1;
-    rc = test_mmrm_client_set_value(client, &client_data, val);
-    if (rc != 0)
-      goto err_setval;
+		// Get value
+		rc = test_mmrm_client_get_value(client, &res_val);
+		if (rc != 0)
+			goto err_getval;
 
-    val = mmrm_test_clk_client_list[index][i].nom;
-    rc = test_mmrm_client_set_value(client, &client_data, val);
-    if (rc != 0)
-      goto err_setval;
+	err_setval:
+	err_getval:
 
-    // Get value
-    rc = test_mmrm_client_get_value(client, &res_val);
-    if (rc != 0)
-      goto err_getval;
+		// Reset clk rate
+		test_mmrm_client_set_value(client, &client_data, 0);
 
-  err_setval:
-  err_getval:
+		// Deregister client
+		rc = test_mmrm_client_deregister(client);
+		if (rc != 0)
+			goto err_deregister;
 
-    // Deregister client
-    rc = test_mmrm_client_deregister(client);
-    if (rc != 0)
-      goto err_deregister;
+	err_clk:
+	err_register:
+	err_deregister:
 
-  err_register:
-  err_deregister:
+		if (rc == 0)
+			pass_count++;
 
-    if (rc == 0)
-      pass_count++;
+		// Free clk
+		if (!IS_ERR_OR_NULL(clk))
+			clk_put(clk);
 
-    // Reset clk
-    desc.client_info.desc.clk = NULL;
-  }
+		// Reset clk
+		desc.client_info.desc.clk = NULL;
+	}
 
-  pr_info("%s: Finish client tests (pass / total): (%d / %d)\n", __func__, pass_count, count);
+	pr_info("%s: Finish client tests (pass / total): (%d / %d)\n", __func__, pass_count, count);
 }
