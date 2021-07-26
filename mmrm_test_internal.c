dd0e1a50b97493126b1a6a0d962583cc92f22d15
mbao <mbao@codeaurora.org>
2021-07-26 16:22:14 -0700

    mmrm_test: Modified testcase to support more level
    Add more clk freq.
    Add more testcases.
    
    Change-Id: Id0366d96419275c5c727a2cb24b9f7b39d4f7778
    Signed-off-by: mbao <mbao@codeaurora.org>

--- mmrm_test_internal.c
+++ mmrm_test_internal.c
@@ -22,6 +22,8 @@
 #define MMRM_TEST_NUM_CASES 3
 
 enum mmrm_test_vdd_level {
+	MMRM_TEST_VDD_LEVEL_LOW_SVS,
+	MMRM_TEST_VDD_LEVEL_SVS,
 	MMRM_TEST_VDD_LEVEL_SVS_L1,
 	MMRM_TEST_VDD_LEVEL_NOM,
 	MMRM_TEST_VDD_LEVEL_TURBO,
@@ -117,87 +119,87 @@ static struct mmrm_test_clk_client mmrm_test_clk_client_list[][MMRM_TEST_MAX_CLK
 	/* WAIPIO */
 	{
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 51, "cam_cc_ife_0_clk_src", NULL},
-			{675000000, 785000000, 785000000},
+			{432000000, 594000000, 675000000, 785000000, 785000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 55, "cam_cc_ife_1_clk_src", NULL},
-			{675000000, 785000000, 785000000},
+			{432000000, 594000000, 675000000, 785000000, 785000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 59, "cam_cc_ife_2_clk_src", NULL},
-			{675000000, 785000000, 785000000},
+			{432000000, 594000000, 675000000, 785000000, 785000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 37, "cam_cc_csid_clk_src", NULL},
-			{480000000, 480000000, 480000000},
+			{400000000, 480000000, 480000000, 480000000, 480000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 116, "cam_cc_sfe_0_clk_src", NULL},
-			{675000000, 785000000, 785000000},
+			{432000000, 594000000, 675000000, 785000000, 785000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 119, "cam_cc_sfe_1_clk_src", NULL},
-			{675000000, 785000000, 785000000},
+			{432000000, 594000000, 675000000, 785000000, 785000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 70, "cam_cc_ipe_nps_clk_src", NULL},
-			{600000000, 700000000, 700000000},
+			{364000000, 500000000, 600000000, 700000000, 700000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 2, "cam_cc_bps_clk_src", NULL},
-			{480000000, 600000000, 600000000}},
+			{200000000, 400000000, 480000000, 600000000, 600000000}},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 64, "cam_cc_ife_lite_clk_src", NULL},
-			{480000000, 480000000, 480000000},
+			{400000000, 480000000, 480000000, 480000000, 480000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 75, "cam_cc_jpeg_clk_src", NULL},
-			{480000000, 600000000, 600000000},
+			{200000000, 400000000, 480000000, 600000000, 600000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 5, "cam_cc_camnoc_axi_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{300000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 67, "cam_cc_ife_lite_csid_clk_src", NULL},
-			{480000000, 480000000, 480000000},
+			{400000000, 480000000, 480000000, 480000000, 480000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 49, "cam_cc_icp_clk_src", NULL},
-			{600000000, 600000000, 600000000},
+			{400000000, 480000000, 600000000, 600000000, 600000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 23, "cam_cc_cphy_rx_clk_src", NULL},
-			{480000000, 480000000, 480000000},
+			{400000000, 480000000, 480000000, 480000000, 480000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 25, "cam_cc_csi0phytimer_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{400000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 27, "cam_cc_csi1phytimer_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{400000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 29, "cam_cc_csi2phytimer_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{400000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 31, "cam_cc_csi3phytimer_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{400000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 33, "cam_cc_csi4phytimer_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{400000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 35, "cam_cc_csi5phytimer_clk_src", NULL},
-			{400000000, 400000000, 400000000},
+			{400000000, 400000000, 400000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 8, "cam_cc_cci_0_clk_src", NULL},
-			{37500000, 37500000, 37500000},
+			{37500000, 37500000, 37500000, 37500000, 37500000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 10, "cam_cc_cci_1_clk_src", NULL},
-			{37500000, 37500000, 37500000},
+			{37500000, 37500000, 37500000, 37500000, 37500000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 123, "cam_cc_slow_ahb_clk_src", NULL},
-			{80000000, 80000000, 80000000},
+			{80000000, 80000000, 80000000, 80000000, 80000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CAMERA, 45, "cam_cc_fast_ahb_clk_src", NULL},
-			{300000000, 400000000, 400000000},
+			{100000000, 200000000, 300000000, 400000000, 400000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_CVP, 8, "video_cc_mvs1_clk_src", NULL},
-			{1500000000, 1650000000, 1650000000},
+			{1050000000, 1350000000, 1500000000, 1650000000, 1650000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_DISPLAY, 61, "disp_cc_mdss_mdp_clk_src", NULL},
-			{375000000, 500000000, 500000000},
+			{200000000, 325000000, 375000000, 500000000, 500000000},
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_DISPLAY, 15, "disp_cc_mdss_dptx0_link_clk_src", NULL},
-			{540000, 810000, 810000}, // kHz
+			{270000000, 270000000, 540000, 810000, 810000}, // kHz
 			NULL},
 		{{MMRM_CLIENT_DOMAIN_VIDEO, 3, "video_cc_mvs0_clk_src", NULL},
-			{1098000000, 1332000000, 1332000000},
+			{720000000, 1014000000, 1098000000, 1332000000, 1332000000},
 			NULL},
 	},
 };
@@ -406,9 +408,9 @@ void test_mmrm_client(struct platform_device *pdev, int index, int count)
 #define WP_CAM_CC_IFE_0_IDX        0
 #define WP_CAM_CC_IFE_1_IDX        1
 #define WP_CAM_CC_IFE_2_IDX        2
-#define WAIPIO_CAM_CC_CSID_CLK_SRC_TEST_IDX         3
-#define WAIPIO_CAM_CC_SFE_0_CLK_SRC_TEST_IDX        4
-#define WAIPIO_CAM_CC_SFE_1_CLK_SRC_TEST_IDX        5
+#define WP_CAM_CC_CSID_IDX         3
+#define WP_CAM_CC_SFE_0_IDX        4
+#define WP_CAM_CC_SFE_1_IDX        5
 #define WP_CAM_CC_IPE_NPS_IDX      6
 #define WP_CAM_CC_BPS_IDX          7
 #define WP_CAM_CC_IFE_LITE_IDX     8
@@ -547,8 +549,8 @@ void test_mmrm_client(struct platform_device *pdev, int index, int count)
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_2_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_LITE_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
-	{&mmrm_test_clk_client_list[n][WAIPIO_CAM_CC_SFE_0_CLK_SRC_TEST_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
-	{&mmrm_test_clk_client_list[n][WAIPIO_CAM_CC_SFE_1_CLK_SRC_TEST_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_SFE_0_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_SFE_1_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_LITE_CSID_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
@@ -583,8 +585,8 @@ void test_mmrm_client(struct platform_device *pdev, int index, int count)
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_2_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_LITE_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
-	{&mmrm_test_clk_client_list[n][WAIPIO_CAM_CC_SFE_0_CLK_SRC_TEST_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
-	{&mmrm_test_clk_client_list[n][WAIPIO_CAM_CC_SFE_1_CLK_SRC_TEST_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_SFE_0_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_SFE_1_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_LITE_CSID_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
 	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
@@ -622,6 +624,123 @@ void test_mmrm_client(struct platform_device *pdev, int index, int count)
 	{NULL, 0} \
 }
 
+//	ife0, ife1 (lowsvs) + ipe (svs) + bps (nom) + mdp (svsl1) + eva (svsl1) + vid (svsl1)
+//	ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (lowsvs) + vid (lowsvs)
+//	ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (lowsvs) + vid (svs)
+//	ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (nom) + vid (lowsvs)
+//	ife0, ife1, ife2 (svs) + ipe (nom) + bps (nom) + sbi (svs) + mdp (svsl1) + eva (svs) + vid (lowsvs)
+//	ife0, ife1 (svs) , ife2 (lowsvs) +
+//	sfe0 (svs) + sfe1(svs) + ipe (nom) + bps (nom) + sbi (svs) + mdp (svsl1) + eva (lowsvs)
+//	ife0, ife1 (svs) + ipe (nom) + bps (nom) + sbi (svs) + mdp (svsl1) + eva (svs) + vid (svs)
+//	ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (lowsvs) + vid (lowsvs)
+//	ife0, ife1 (lowsvs) + ipe (svs) + bps (nom) + mdp (svsl1) + eva (svsl1) + vid (svsl1)
+
+#define  decl_test_case_11(n) static struct mmrm_test_desc test_case_11_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (lowsvs) + vid (lowsvs)
+#define  decl_test_case_12(n) static struct mmrm_test_desc test_case_12_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (lowsvs) + vid (svs)
+#define  decl_test_case_13(n) static struct mmrm_test_desc test_case_13_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (nom) + vid (lowsvs)
+#define  decl_test_case_14(n) static struct mmrm_test_desc test_case_14_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1, ife2 (svs) + ipe (nom) + bps (nom) + sbi (svs) + mdp (svsl1) + eva (svs) + vid (lowsvs)
+#define  decl_test_case_15(n) static struct mmrm_test_desc test_case_15_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_2_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1 (svs) , ife2 (lowsvs) + sfe0 (svs) + sfe1(svs) + ipe (nom) +
+//		bps (nom) + sbi (svs) + mdp (svsl1) + eva (lowsvs)
+#define  decl_test_case_16(n) static struct mmrm_test_desc test_case_16_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_2_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_SFE_0_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_SFE_0_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1 (svs) + ipe (nom) + bps (nom) + sbi (svs) + mdp (svsl1) + eva (svs) + vid (svs)
+#define  decl_test_case_17(n) static struct mmrm_test_desc test_case_17_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_SVS},\
+	{NULL, 0} \
+}
+
+//		ife0, ife1 (lowsvs) + ipe (nom) + bps (nom) + sbi (lowsvs) + mdp (svsl1) + eva (lowsvs) + vid (lowsvs)
+#define  decl_test_case_18(n) static struct mmrm_test_desc test_case_18_##n[] = {\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IFE_1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_IPE_NPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	{&mmrm_test_clk_client_list[n][WP_CAM_CC_BPS_IDX], MMRM_TEST_VDD_LEVEL_NOM},\
+	\
+	{&mmrm_test_clk_client_list[n][WP_DISP_CC_MDSS_MDP_IDX], MMRM_TEST_VDD_LEVEL_SVS_L1},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS1_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{&mmrm_test_clk_client_list[n][WP_VIDEO_CC_MVS0_IDX], MMRM_TEST_VDD_LEVEL_LOW_SVS},\
+	{NULL, 0} \
+}
+
 
 decl_test_case_1(MMRM_TEST_WAIPIO);
 decl_test_case_2(MMRM_TEST_WAIPIO);
@@ -632,6 +751,15 @@ decl_test_case_6(MMRM_TEST_WAIPIO);
 decl_test_case_7(MMRM_TEST_WAIPIO);
 decl_test_case_8(MMRM_TEST_WAIPIO);
 
+decl_test_case_11(MMRM_TEST_WAIPIO);
+decl_test_case_12(MMRM_TEST_WAIPIO);
+decl_test_case_13(MMRM_TEST_WAIPIO);
+decl_test_case_14(MMRM_TEST_WAIPIO);
+decl_test_case_15(MMRM_TEST_WAIPIO);
+decl_test_case_16(MMRM_TEST_WAIPIO);
+decl_test_case_17(MMRM_TEST_WAIPIO);
+decl_test_case_18(MMRM_TEST_WAIPIO);
+
 struct  mmrm_test_desc  *waipio_all_testcases[] = {
 	test_case_1_MMRM_TEST_WAIPIO,
 	test_case_2_MMRM_TEST_WAIPIO,
@@ -641,6 +769,15 @@ struct  mmrm_test_desc  *waipio_all_testcases[] = {
 	test_case_6_MMRM_TEST_WAIPIO,
 	test_case_7_MMRM_TEST_WAIPIO,
 	test_case_8_MMRM_TEST_WAIPIO,
+
+	test_case_11_MMRM_TEST_WAIPIO,
+	test_case_12_MMRM_TEST_WAIPIO,
+	test_case_13_MMRM_TEST_WAIPIO,
+	test_case_14_MMRM_TEST_WAIPIO,
+	test_case_15_MMRM_TEST_WAIPIO,
+	test_case_16_MMRM_TEST_WAIPIO,
+	test_case_17_MMRM_TEST_WAIPIO,
+	test_case_18_MMRM_TEST_WAIPIO,
 };
 
 int waipio_all_testcases_count = sizeof(waipio_all_testcases)/sizeof(waipio_all_testcases[0]);
