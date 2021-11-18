77ae3f31f06e55407773e9546e607972164880d6
Ingrid Gallardo <quic_ingridg@quicinc.com>
2021-11-17 17:16:04 -0800

    mm-drivers: hw_fence: Add support for hw-fence driver
    This change adds support for the hw-fence driver that initialize, expose
    and manage the interfaces for the hw-fences, which are the synchronization
    primitives to allow the hardware to hardware signalization of the fences
    for the frame buffers shared between gpu and display hw-cores.
    
    Change-Id: If2313585d5a9f3ac90e16aad3464600641a6fa04
    Signed-off-by: Ingrid Gallardo <quic_ingridg@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 MM_DRIVER_PATH := $(call my-dir)
 include $(MM_DRIVER_PATH)/msm_ext_display/Android.mk
+include $(MM_DRIVER_PATH)/hw_fence/Android.mk
 ifneq ($(TARGET_BOARD_PLATFORM), taro)
 include $(MM_DRIVER_PATH)/sync_fence/Android.mk
 endif
--- mm_driver_product.mk
+++ mm_driver_product.mk
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
-PRODUCT_PACKAGES += msm_ext_display.ko
+PRODUCT_PACKAGES += msm_ext_display.ko msm_hw_fence.ko
 
 ifneq ($(TARGET_BOARD_PLATFORM), taro)
 PRODUCT_PACKAGES += sync_fence.ko
