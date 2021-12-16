559a6bee2c2b577f3790475d8f2a4f43c94a2627
Jeykumar Sankaran <quic_jeykumar@quicinc.com>
2021-12-15 16:29:11 -0800

    mm-drivers: enable mm-driver modules compilation
    Enable compilation of mm-driver modules and add scripts to copy
    the uapi header files.
    
    Change-Id: I0af6581ca96aa630c9707ef05abc4cccbfe92bab
    Signed-off-by: Jeykumar Sankaran <quic_jeykumar@quicinc.com>

---
+++ mm_driver_board.mk
@@ -0,0 +1,12 @@
+#SPDX-License-Identifier: GPL-2.0-only
+
+ifneq ($(TARGET_BOARD_AUTO),true)
+	ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
+		BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_ext_display.ko \
+					       $(KERNEL_MODULES_OUT)/sync_fence.ko
+		BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_ext_display.ko \
+					       	       $(KERNEL_MODULES_OUT)/sync_fence.ko
+		BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/msm_ext_display.ko \
+					                             $(KERNEL_MODULES_OUT)/sync_fence.ko
+	endif
+endif
