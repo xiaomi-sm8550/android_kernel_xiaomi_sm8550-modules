7398cbd4c4273662677123dc3da86b51f387c271
Gaurav Kashyap <quic_gaurkash@quicinc.com>
2022-03-16 15:21:16 -0700

    securemsm-kernel : Correction in LF Copyright for qrng.
    Change-Id: I1f0b5a2b39a1bd0207a9cedcb85f5ef53e843e86

--- qrng/msm_rng.c
+++ qrng/msm_rng.c
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2011-2013, 2015, 2017-2022 The Linux Foundation. All rights
+ * Copyright (c) 2011-2013, 2015, 2017-2021 The Linux Foundation. All rights
  * reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/kernel.h>
 #include <linux/module.h>
