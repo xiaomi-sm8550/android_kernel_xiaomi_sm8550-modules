5b5214916c3fe0f104bdf59f5ca61b93e928051f
Jeykumar Sankaran <quic_jeykumar@quicinc.com>
2022-04-01 19:07:41 -0700

    mm-drivers: sync_fence: avoid compiling spec_fence driver for taro
    sync_fence driver is maintained in kernel SI for all the taro variants.
    Since the Display SI 3.0 is shared with taro dev SI variant,
    avoid compiling sync_fence as dlkm for taro target.
    
    Change-Id: Icc7990812256a42efad7a8945c08338f83ee0914
    Signed-off-by: Jeykumar Sankaran <quic_jeykumar@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -1,1 +1,6 @@
-include $(call all-subdir-makefiles)
+MM_DRIVER_PATH := $(call my-dir)
+include $(MM_DRIVER_PATH)/msm_ext_display/Android.mk
+ifneq ($(TARGET_BOARD_PLATFORM), taro)
+include $(MM_DRIVER_PATH)/sync_fence/Android.mk
+endif
+
