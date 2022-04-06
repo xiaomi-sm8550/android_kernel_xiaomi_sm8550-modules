abb78cf97c68f1c73237e4977c0ddd869ee8c7eb
Bruce Levy <quic_blevy@quicinc.com>
2022-04-05 19:16:42 -0700

    securemsm-kernel: Fix product_packages entry
    Fix product_package entry for module list.
    
    Change-Id: I48acc82fca94fc2f8c4547d5d1c600e62fa8c114

--- securemsm_kernel_product_board.mk
+++ securemsm_kernel_product_board.mk
@@ -1,11 +1,12 @@
 #Build ssg kernel driver
-PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/smcinvoke_dlkm.ko \
-  $(KERNEL_MODULES_OUT)/tz_log_dlkm.ko \
-  $(KERNEL_MODULES_OUT)/qcedev-mod_dlkm.ko \
-  $(KERNEL_MODULES_OUT)/qce50_dlkm.ko \
-  $(KERNEL_MODULES_OUT)/qcrypto-msm_dlkm.ko \
-  $(KERNEL_MODULES_OUT)/hdcp_qseecom_dlkm.ko \
-  $(KERNEL_MODULES_OUT)/qrng_dlkm.ko \
+
+PRODUCT_PACKAGES += smcinvoke_dlkm.ko
+PRODUCT_PACKAGES += tz_log_dlkm.ko
+PRODUCT_PACKAGES += qcedev-mod_dlkm.ko
+PRODUCT_PACKAGES += qce50_dlkm.ko
+PRODUCT_PACKAGES += qcrypto-msm_dlkm.ko
+PRODUCT_PACKAGES += hdcp_qseecom_dlkm.ko
+PRODUCT_PACKAGES += qrng_dlkm.ko
 
 
 
