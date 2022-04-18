e8d76601a79c07fda1410321b0140fc9ef49434c
Manikandan Mohan <quic_manikand@quicinc.com>
2022-04-18 11:32:29 -0700

    cnss2: Fix PBL / SBL logs collection for KIWI device
    Fix PBL / SBL logs collection for KIWI device.
    
    Change-Id: I271f1dc605f49db60339f1d7470bdd6c60770eb2

--- cnss2/pci.c
+++ cnss2/pci.c
@@ -1801,6 +1801,7 @@ static void cnss_pci_dump_bl_sram_mem(struct cnss_pci_data *pci_priv)
 		pbl_log_sram_start = KIWI_DEBUG_PBL_LOG_SRAM_START;
 		pbl_log_max_size = KIWI_DEBUG_PBL_LOG_SRAM_MAX_SIZE;
 		sbl_log_max_size = KIWI_DEBUG_SBL_LOG_SRAM_MAX_SIZE;
+		break;
 	default:
 		return;
 	}
