ccc44aff77f9cdf56595ce4bbba894b770b49523
Jayasri Bhattacharyya <quic_jbhattac@quicinc.com>
2022-07-14 12:39:55 +0530

    Securemsm-kernel: Enable qseecom for khaje
    1. Add qseecom_dlkm.ko based on Khaje and AUTO arch type
    2. Restructure conf file into multiple conf files
       to enable feature based conf file.
    
    Change-Id: I8bc0472667aebc35e8d1afa37eaca5c3353fd191
    Signed-off-by: Jayasri Bhattacharyya <quic_jbhattac@quicinc.com>

--- securemsm_kernel_product_board.mk
+++ securemsm_kernel_product_board.mk
@@ -9,18 +9,18 @@ ENABLE_SECUREMSM_DLKM := true
 endif
 
 ifeq ($(ENABLE_SECUREMSM_DLKM), true)
-PRODUCT_PACKAGES += smcinvoke_dlkm.ko
 PRODUCT_PACKAGES += tz_log_dlkm.ko
 PRODUCT_PACKAGES += qcedev-mod_dlkm.ko
 PRODUCT_PACKAGES += qce50_dlkm.ko
 PRODUCT_PACKAGES += qcrypto-msm_dlkm.ko
 PRODUCT_PACKAGES += hdcp_qseecom_dlkm.ko
 PRODUCT_PACKAGES += qrng_dlkm.ko
-ifeq ($(TARGET_BOARD_AUTO),true)
-PRODUCT_PACKAGES += qseecom_dlkm.ko
-else
 PRODUCT_PACKAGES += smcinvoke_dlkm.ko
-endif #TARGET_BOARD_AUTO
+
+#Enable Qseecom if TARGET_ENABLE_QSEECOM or TARGET_BOARD_AUTO is set to true
+ifneq (, $(filter true, $(TARGET_ENABLE_QSEECOM) $(TARGET_BOARD_AUTO)))
+PRODUCT_PACKAGES += qseecom_dlkm.ko
+endif #TARGET_ENABLE_QSEECOM OR TARGET_BOARD_AUTO
 
 endif #ENABLE_SECUREMSM_DLKM
 
