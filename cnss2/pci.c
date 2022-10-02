f7e10ed87e7487e7c7fd7a02a3d2873df907d787
Rajesh Chauhan <quic_rajeshc@quicinc.com>
2022-10-02 14:00:40 -0700

    cnss2: fix issue in return type of debug functions
    Fix issue in return type of following functions:
     - cnss_rddm_trigger_debug()
     - cnss_rddm_trigger_check()
    
    Change-Id: I41eb4637605f9a2189e03e732c688c25b0895a90
    CRs-Fixed: 3304957

--- cnss2/pci.c
+++ cnss2/pci.c
@@ -1691,7 +1691,7 @@ static int cnss_pci_check_mhi_state_bit(struct cnss_pci_data *pci_priv,
 	return -EINVAL;
 }
 
-static cnss_rddm_trigger_debug(struct cnss_pci_data *pci_priv)
+static int cnss_rddm_trigger_debug(struct cnss_pci_data *pci_priv)
 {
 	int read_val, ret;
 
@@ -1715,7 +1715,7 @@ static cnss_rddm_trigger_debug(struct cnss_pci_data *pci_priv)
 	return ret;
 }
 
-static cnss_rddm_trigger_check(struct cnss_pci_data *pci_priv)
+static int cnss_rddm_trigger_check(struct cnss_pci_data *pci_priv)
 {
 	int read_val, ret;
 
