cd824be07638ea247c314a21952d10528b45b241
Harshdeep Dhatt <quic_hdhatt@quicinc.com>
2022-10-07 16:09:27 -0600

    msm: kgsl: Check jiffies before SLUMBER entry
    There is a race condition where the idle timer executes right after
    when dispatcher decrements the active count to 0, without waiting for
    80 ms(which is the current idle timer duration). Race condition:
    Say, everything is idle for 80 ms, and the idle timer gets
    scheduled to be executed. At the same time, a new submission comes
    in, gets submitted and retired, and at the same time the idle timer
    function also starts executing and waits for device mutex. Now, as
    soon as the dispatcher decrements the active count and unlocks device
    mutex, the idle timer gets the mutex and attempts SLUMBER. However, it
    finds GMU isn't idle(busy with TS_RETIRE, or IFPC sequence) and
    triggers snapshot/recovery. Fix this by making sure that idle timer
    function honors the latest idle_jiffies value before triggering SLUMBER
    entry.
    
    Change-Id: Iee0743b41afa616d8a0d1df19f9ae365748d06ce
    Signed-off-by: Harshdeep Dhatt <quic_hdhatt@quicinc.com>
    Signed-off-by: Deepak Kumar <quic_dkumar@quicinc.com>

--- adreno_a6xx_gmu.c
+++ adreno_a6xx_gmu.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <dt-bindings/regulator/qcom,rpmh-regulator-levels.h>
@@ -3330,7 +3330,7 @@ static void gmu_idle_check(struct work_struct *work)
 	if (test_bit(GMU_DISABLE_SLUMBER, &device->gmu_core.flags))
 		goto done;
 
-	if (atomic_read(&device->active_cnt)) {
+	if (atomic_read(&device->active_cnt) || time_is_after_jiffies(device->idle_jiffies)) {
 		kgsl_pwrscale_update(device);
 		kgsl_start_idle_timer(device);
 		goto done;
