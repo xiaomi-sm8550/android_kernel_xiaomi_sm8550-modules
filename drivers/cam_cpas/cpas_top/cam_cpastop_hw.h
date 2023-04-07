ff278c7e8b74e385b0ee670c75b65ec5b27d2d46
Karthik Dillibabu <quic_kard@quicinc.com>
2023-04-07 16:25:56 +0530

    msm: camera: common: validation of IFE, SFE and CUSTOM support
    This change validates whether a particular IFE, SFE and
    CUSTOM is supported or not. Based on this, probe of IFE,
    SFE and CUSTOM driver will happen accordingly.
    
    CRs-Fixed: 3438704
    Change-Id: I5418661ff092fb15d04ab7dda5f4b59742a232a8
    Signed-off-by: Karthik Dillibabu <quic_kard@quicinc.com>

--- drivers/cam_cpas/cpas_top/cam_cpastop_hw.h
+++ drivers/cam_cpas/cpas_top/cam_cpastop_hw.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _CAM_CPASTOP_HW_H_
@@ -9,6 +9,10 @@
 
 #include "cam_cpas_api.h"
 #include "cam_cpas_hw.h"
+
+/* Camera Hw parts array indices */
+#define CAM_CPAS_PART_MAX_FUSE_BITS 8
+#define CAM_CPAS_PART_MAX_FUSE_BIT_INFO 2
 
 /**
  * enum cam_camnoc_hw_irq_type - Enum for camnoc error types
@@ -293,6 +297,18 @@ struct cam_cpas_hw_errata_wa {
 };
 
 /**
+ * struct cam_cpas_subpart_info : Struct for camera Hw parts info
+ *
+ * @num_bits: Number of entries in hw_bitmap_mask
+ * @hw_bitmap_mask: Contains Fuse flag and hw_map info
+ *
+ */
+struct cam_cpas_subpart_info {
+	uint32_t num_bits;
+	uint32_t hw_bitmap_mask[CAM_CPAS_PART_MAX_FUSE_BITS][CAM_CPAS_PART_MAX_FUSE_BIT_INFO];
+};
+
+/**
  * struct cam_cpas_hw_errata_wa_list : List of HW Errata workaround info
  *
  * @camnoc_flush_slave_pending_trans: Errata workaround info for flushing
@@ -357,6 +373,7 @@ struct cam_cpas_test_irq_info {
  * @irq_err_size: Array size of IRQ Error settings
  * @err_logger: Pointer to CAMNOC IRQ Error logger read registers
  * @errata_wa_list: HW Errata workaround info
+ * @cam_subpart_info: camera parts fuse description
  * @test_irq_info: CAMNOC Test IRQ info
  *
  */
@@ -368,6 +385,7 @@ struct cam_camnoc_info {
 	int irq_err_size;
 	struct cam_camnoc_err_logger_info *err_logger;
 	struct cam_cpas_hw_errata_wa_list *errata_wa_list;
+	struct cam_cpas_subpart_info *cam_subpart_info;
 	struct cam_cpas_test_irq_info test_irq_info;
 };
 
