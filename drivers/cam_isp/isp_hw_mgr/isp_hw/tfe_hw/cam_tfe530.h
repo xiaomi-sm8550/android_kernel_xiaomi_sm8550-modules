938c8d60309c5c96803b0d183b5565fb5f50cf76
Dharmender Sharma <quic_dharshar@quicinc.com>
2023-05-17 04:42:53 +0530

    msm: camera: tfe: Add support for TFE BW limiter
    Add blob to allow userland to configure BW limter for TFE WMs.
    
    CRs-Fixed: 3505174
    Change-Id: I5c671cbcfa5557082c4ae5302471a2ece45294ab
    Signed-off-by: Dharmender Sharma <quic_dharshar@quicinc.com>

--- drivers/cam_isp/isp_hw_mgr/isp_hw/tfe_hw/cam_tfe530.h
+++ drivers/cam_isp/isp_hw_mgr/isp_hw/tfe_hw/cam_tfe530.h
@@ -876,6 +876,9 @@ static struct cam_tfe_bus_hw_info  tfe530_bus_hw_info = {
 	.rdi_width = 64,
 	.mode_cfg_shift = 16,
 	.height_shift = 16,
+	.max_bw_counter_limit  = 0xFF,
+	.counter_limit_shift   = 1,
+	.counter_limit_mask    = 0xF,
 };
 
 struct cam_tfe_hw_info cam_tfe530 = {
