ba6b9d759462d68c62bf1a6a643478434a96bc97
Amarender Kanchugatla <quic_akanchug@quicinc.com>
2023-06-22 13:03:19 +0530

    Enabling BT Kernel compilation support for kona trinket
    Change-Id: I55556c000cc02801419fb5c5ee9d07a8543f62e1

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@
 LOCAL_PATH := $(call my-dir)
 
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,taro kalama bengal crow kona), true)
+ifeq ($(call is-board-platform-in-list,taro kalama bengal crow kona trinket), true)
 
 BT_SELECT := CONFIG_MSM_BT_POWER=m
 #ifdef CONFIG_SLIMBUS
